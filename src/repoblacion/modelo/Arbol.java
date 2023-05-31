b10fa79d9a0f2a89552438ae70d5a1f5487804eb
José <Jos@DESKTOP-61Q5ISJ.home>
2023-05-31 13:53:36 +0200

    Clase Arbol creada

--- src/repoblacion/modelo/Arbol.java
+++ src/repoblacion/modelo/Arbol.java
@@ -1,5 +1,42 @@
 package repoblacion.modelo;
 
 public class Arbol {
+	private Posicion posicion;
+	private Especie especie;
+	
+	public Arbol(Posicion p, Especie e) {
+		setEspecie(e);
+		setPosicion(p);
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(Posicion posicion) {
+		if(posicion!=null) {
+			this.posicion = posicion;
 
+		}else {
+			throw new NullPointerException("Poscion no puede ser nula.");
+		}
+	}
+	
+	public Especie getEspecie() {
+		return especie;
+	}
+	public void setEspecie(Especie especie) {
+		if(especie!=null) {
+			this.especie = especie;
+
+		}else {
+			throw new NullPointerException("Especie no puede ser nula.");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "(" + this.getEspecie() + ", " + this.getPosicion() + ")";
+	}
+	
+	
 }
